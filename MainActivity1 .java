9e98cc4dc74e83f59f34d4b9019453a1071ebfe1
Rajashekarg12 <110624878+Rajashekarg12@users.noreply.github.com>
2023-12-20 15:56:18 +0530

    Update MainActivity1 .java

--- MainActivity1 .java
+++ MainActivity1 .java
@@ -1,6 +1,36 @@
-public class CookieExample {
-    public void setCookie(String userToken) {
-        Cookie myCookie = new Cookie('userToken', userToken, null, -1, false);
-        ApexPages.currentPage().setCookies(new Cookie[]{myCookie});
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserAuthentication {
+
+    public boolean authenticateUser(String username, String password) {
+        boolean isAuthenticated = false;
+
+        // Example SQL query vulnerable to SQL injection
+        String sqlQuery = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'";
+
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+
+            if (resultSet.next()) {
+                isAuthenticated = true;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return isAuthenticated;
     }
+
+    private Connection getConnection() throws SQLException {
+        // Implementing the connection setup (this is a simplified example)
+        // You should use connection pooling and other security best practices
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/mydatabase", "user", "password");
+    }
+
+    // Other methods and functionalities in the class...
 }
